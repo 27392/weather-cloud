37a1365cfbd50f2f62224a6f6dba4bc8d2f33f21
liwenhao <2739227392@qq.com>
2018-09-27 15:50:18 +0800

    添加数据采集服务

--- README.md
+++
@@ -1,2 +0,0 @@
-# weather-cloud
-⛅天气预报
--- src/main/resources/static/js/weather/report.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#selectCityId").change(function(){
-        var cityId = $(this).val();
-        var url = '/report/cityId/' + cityId;
-        window.location.href = url;
-    })
-})
