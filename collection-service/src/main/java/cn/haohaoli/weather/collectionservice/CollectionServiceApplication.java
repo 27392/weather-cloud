b1689c0b1aacdceac5e7a3e8e9f3ac5e10d98fbd
liwenhao <2739227392@qq.com>
2018-09-27 15:52:09 +0800

    添加数据采集服务

---
+++ collection-service/src/main/java/cn/haohaoli/weather/collectionservice/CollectionServiceApplication.java
@@ -0,0 +1,12 @@
+package cn.haohaoli.weather.collectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionServiceApplication.class, args);
+    }
+}
